e2c3698d4fda75c9ea121ef116b197b24d6e0e35
zzes <kuanglong0312@sina.com>
2017-07-13 11:29:31 +0800

    webpack config file

--- st2/webpack.config.js
+++ st2/webpack.config.js
@@ -1,16 +1,17 @@
 const path = require('path');
-const HtmlWebpackPlugin  = require('html-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
     entry: {
         app: './src/index.js',
         print: './src/print.js',
         vendor: ['lodash']
     },
-    plugins:[
-    	new HtmlWebpackPlugin({
-    		title: "Output Management"
-    	})
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: "Output Management klrentest"
+        })
     ],
     output: {
         filename: '[name].bundle.js',
